c1ef62f4cec9a42eb6991a8b34a228f7def9061c
Laura Carroll <lmc297@cornell.edu>
2018-02-15 17:00:30 +0100

    Update snpbac.rb

--- snpbac.rb
+++ snpbac.rb
@@ -13,24 +13,10 @@ class Snpbac < Formula
   depends_on "bwa"
   depends_on "bowtie2"
   
-  #resource "biopython" do
-  #  url "http://biopython.org/DIST/biopython-1.69.tar.gz"
-  #  sha256 "169ffa90c3d3ec5678c7a5c99501c0cfeb54c40ca51a619ce6cee5026d3403eb"
-  #end
-  
   bottle :unneeded 
    
   def install
     bin.install "snpbac", "make_snpbac_infile.py", "mapping.py", "__init__.py", "call_variants.py", "recombination.py", "snp_matrix.py"
-    #ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    #ENV.prepend "PYTHONPATH", libexec, ':'
-    #ENV.prepend "PYTHONPATH", libexec/"src", ':'
-    #puts "done with env"
-    # for python_package in ["biopython"]
-    #resource("biopython").stage do
-    #  system "python", *Language::Python.setup_install_args(libexec)
-    #end
-
-
+  
   end
 end
